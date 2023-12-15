6c0966fc4ec206649ea6422098adf44b6ae6ee23
David Foster <david.foster@techsmart.codes>
2023-12-15 11:58:31 -0800

    Create empty app: code

--- apps/code/urls.py
+++ apps/code/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from apps.code import views
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # Apps
-    path('code/', include('apps.code.urls')),
-    
-    # Admin-only
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home')
 ]
--- apps/main/settings.py
+++ apps/main/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django builtin
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Apps
+    'apps.code',
 ]
 
 MIDDLEWARE = [
