680b5b294dbc1962dc64912dcad8cffa24b398f3
David Foster <david.foster@techsmart.codes>
2024-01-12 10:41:47 -0800

    Ensure can reference .png file from .js file

--- static/code/panels/project.js
+++ static/code/panels/project.js
@@ -4,6 +4,13 @@
 // cannot show the line number where the error occurred in the original CSS file.
 import '@ts/code/panels/project.css';
 
+// URLs for assets (like images) can be imported in JS directly.
+import boulderImageUrl from '@ts/code/imgs/boulderjoy.png';
+
 export function setupProjectPanel() {
     console.log('setupProjectPanel called');
+    
+    const imageDom = document.createElement('img');
+    imageDom.src = boulderImageUrl;
+    document.querySelector('#ts-js-image').appendChild(imageDom);
 }
